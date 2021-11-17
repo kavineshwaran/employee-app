72848750f86a5b927497788d109cd615b647289b
Ubuntu <ubuntu@ip-172-31-21-218.us-east-2.compute.internal>
2021-11-17 06:21:02 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :employee_details
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  root 'employee_details#index'
+  end
